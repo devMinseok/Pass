17254ee0524313dc7ca8f742419fcebc120936f3
KangMinseok <qbq5000@gmail.com>
2021-04-30 18:11:44 +0900

    ✨ Util 파일 추가

---
+++ Pass/Source/Util/String.swift
@@ -0,0 +1,22 @@
+//
+//  String.swift
+//  Pass
+//
+//  Created by 강민석 on 2021/04/30.
+//
+
+import UIKit
+
+extension String {
+    func attributing(_ ranges: [String], color: UIColor?) -> NSMutableAttributedString? {
+        guard let color = color else { return nil }
+        
+        let attributedString = NSMutableAttributedString(string: self)
+        
+        ranges.forEach { range in
+            attributedString.addAttribute(.foregroundColor, value: color, range: (self as NSString).range(of: range))
+        }
+        
+        return attributedString
+    }
+}
