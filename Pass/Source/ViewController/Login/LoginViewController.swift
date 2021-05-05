799801a979bc4c9aad0c146f2219b7b60727ba70
KangMinseok <qbq5000@gmail.com>
2021-05-05 23:19:09 +0900

    ✨ 로그인 화면 UI 및 기능 추가

--- Pass/Source/ViewController/Login/LoginViewController.swift
+++ Pass/Source/ViewController/Login/LoginViewController.swift
@@ -2,16 +2,56 @@
 //  LoginViewController.swift
 //  Pass
 //
-//  Created by 강민석 on 2021/04/13.
+//  Created by 강민석 on 2021/04/30.
 //
 
 import UIKit
+
 import ReactorKit
 
-class LoginViewController: BaseViewController, View {
+final class LoginViewController: BaseViewController, View {
     
     typealias Reactor = LoginViewReactor
     
+    // MARK: Constants
+    struct Metric {
+        static let titleLabelTop = 90.f
+        static let titleLabelLeftRight = 35.f
+        
+        static let textFieldLeftRight = 20.f
+        static let textFieldTop = 70.f
+        static let textFieldHeight = 65.f
+        
+        static let checkButtonLeftRight = 20.f
+        static let checkButtonBottom = 10.f
+        static let checkButtonHeight = 55.f
+    }
+    
+    struct Font {
+        static let titleLabel = UIFont.systemFont(ofSize: 24, weight: .bold)
+    }
+    
+    // MARK: - Properties
+    
+    // MARK: - UI
+    let titleLabel = UILabel().then {
+        $0.text = "이메일을 입력해주세요."
+        $0.font = Font.titleLabel
+        $0.numberOfLines = 0
+        $0.textAlignment = .left
+    }
+    
+    let nextButton = PassPlainButton().then {
+        $0.setTitle("확인", for: .normal)
+    }
+    
+    let emailTextField = PassTextField().then {
+        $0.textField.keyboardType = .emailAddress
+        $0.titleLabel.text = "이메일 주소"
+        $0.textField.placeholder = "이메일 주소"
+    }
+    
+    // MARK: - Initializing
     init(
         reactor: Reactor
     ) {
@@ -23,17 +63,62 @@ class LoginViewController: BaseViewController, View {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.addSubview(self.titleLabel)
+        self.view.addSubview(self.nextButton)
+        self.view.addSubview(self.emailTextField)
     }
     
     override func setupConstraints() {
+        self.titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(Metric.titleLabelTop)
+            make.centerX.equalToSuperview()
+            make.left.equalTo(Metric.titleLabelLeftRight)
+            make.right.equalTo(-Metric.titleLabelLeftRight)
+        }
         
+        self.emailTextField.snp.makeConstraints { make in
+            make.top.equalTo(self.titleLabel.snp.bottom).offset(Metric.textFieldTop)
+            make.left.equalTo(self.view.snp.left).offset(Metric.textFieldLeftRight)
+            make.right.equalTo(self.view.snp.right).offset(-Metric.textFieldLeftRight)
+            make.height.equalTo(Metric.textFieldHeight)
+        }
+        
+        self.nextButton.snp.makeConstraints { make in
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide).offset(-Metric.checkButtonBottom)
+            make.centerX.equalToSuperview()
+            make.left.equalTo(Metric.checkButtonLeftRight)
+            make.right.equalTo(-Metric.checkButtonLeftRight)
+            make.height.equalTo(Metric.checkButtonHeight)
+        }
     }
     
+    // MARK: - Configuring
     func bind(reactor: Reactor) {
         
+        // MARK: - input
+        self.emailTextField.textField.rx.text.orEmpty
+            .map { Reactor.Action.setEmail($0) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        self.nextButton.rx.tap
+            .map { Reactor.Action.next(self.emailTextField.textField.text ?? "") }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        // MARK: - output
+        reactor.state.map { $0.validationResult }
+            .distinctUntilChanged()
+            .bind(to: self.nextButton.rx.error)
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.validationResult }
+            .distinctUntilChanged()
+            .bind(to: emailTextField.rx.error)
+            .disposed(by: disposeBag)
     }
-    
 }
