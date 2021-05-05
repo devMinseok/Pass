c7f216b4e116e988140ca897176a018b4deaf94b
KangMinseok <qbq5000@gmail.com>
2021-05-05 23:18:40 +0900

    ✨ 비밀번호 입력 화면 UI 및 기능 추가

--- Pass/Source/ViewController/Password/PasswordViewReactor.swift
+++ Pass/Source/ViewController/Password/PasswordViewReactor.swift
@@ -2,21 +2,37 @@
 //  PasswordViewReactor.swift
 //  Pass
 //
-//  Created by 강민석 on 2021/04/28.
+//  Created by 강민석 on 2021/05/01.
 //
 
 import ReactorKit
 import RxCocoa
 import RxSwift
+import RxFlow
 
-final class PasswordViewReactor: Reactor {
+final class PasswordViewReactor: Reactor, Stepper {
+    var steps = PublishRelay<Step>()
     
-    typealias Action = NoAction
+    enum Action {
+        case callBack(String)
+    }
+    
+    enum Mutation {
+        
+    }
     
     struct State {
         
     }
     
     let initialState: State = State()
-
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case let .callBack(password):
+            inputPassword.onNext(password)
+            self.steps.accept(PassStep.dismiss)
+            return .empty()
+        }
+    }
 }
