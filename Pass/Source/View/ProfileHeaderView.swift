ebf8e31d11001f8a9795b0d697a95be0d8b6d520
KangMinseok <qbq5000@gmail.com>
2021-05-13 18:20:45 +0900

    💄 ProfileHeaderView 추가
    + 홈화면의 프로필 화면을 tableview header로 넣기위해 추가

---
+++ Pass/Source/View/ProfileHeaderView.swift
@@ -0,0 +1,58 @@
+//
+//  ProfileHeaderView.swift
+//  Pass
+//
+//  Created by 강민석 on 2021/05/13.
+//
+
+import UIKit
+
+import ReactorKit
+
+final class ProfileHeaderView: BaseViewController, View {
+    
+    typealias Reactor = ProfileHeaderViewReactor
+    
+    // MARK: Constants
+    struct Metric {
+        
+    }
+    
+    struct Font {
+        
+    }
+
+    // MARK: - Properties
+
+    // MARK: - UI
+
+    // MARK: - Initializing
+    init(
+        reactor: Reactor
+    ) {
+        defer { self.reactor = reactor }
+        super.init()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func setupConstraints() {
+        
+    }
+
+    // MARK: - Configuring
+    func bind(reactor: Reactor) {
+        // MARK: - input
+        
+        // MARK: - output
+        
+    }
+}
