26e045ad54237c3ceaadbaffecfa5504c8a1ffce
KangMinseok <qbq5000@gmail.com>
2021-05-13 17:50:41 +0900

    💄 계좌 화면 추가

---
+++ Pass/Source/ViewController/Account/AccountViewController.swift
@@ -0,0 +1,58 @@
+//
+//  AccountViewController.swift
+//  Pass
+//
+//  Created by 강민석 on 2021/05/13.
+//
+
+import UIKit
+
+import ReactorKit
+
+final class AccountViewController: BaseViewController, View {
+    
+    typealias Reactor = AccountViewReactor
+    
+    // MARK: Constants
+    struct Metric {
+        
+    }
+    
+    struct Font {
+        
+    }
+
+    // MARK: - Properties
+
+    // MARK: - UI
+
+    // MARK: - Initializing
+    init(
+        reactor: Reactor
+    ) {
+        defer { self.reactor = reactor }
+        super.init()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func setupConstraints() {
+        
+    }
+
+    // MARK: - Configuring
+    func bind(reactor: Reactor) {
+        // MARK: - input
+        
+        // MARK: - output
+        
+    }
+}
