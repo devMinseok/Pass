3eb44e406f8660c0af56807b4f51ba9b2cf63219
KangMinseok <qbq5000@gmail.com>
2021-05-14 15:36:14 +0900

    🎨 모든 서비스를 AppServices 구조체로 관리

---
+++ Pass/Source/AppServices.swift
@@ -0,0 +1,33 @@
+//
+//  AppServices.swift
+//  Pass
+//
+//  Created by 강민석 on 2021/04/06.
+//
+
+import Kingfisher
+import RxViewController
+import RxOptional
+import Swinject
+import SnapKit
+import Then
+
+struct AppServices {
+    let authService: AuthServiceType
+    let userService: UserServiceType
+    let accountService: AccountServiceType
+    
+    init() {
+        self.authService = AuthService()
+        
+        let network = Network<PassAPI>(
+            plugins: [
+                RequestLoggingPlugin(),
+                AuthPlugin(authService: authService)
+            ]
+        )
+        
+        self.userService = UserService(network: network)
+        self.accountService = AccountService(network: network)
+    }
+}
