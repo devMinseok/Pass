a63ce97b8184e960a702d184b824730b823b6b17
KangMinseok <qbq5000@gmail.com>
2021-05-25 09:52:37 +0900

    🚚 Cell 폴더 분리
    + TableView와 CollectionView로

---
+++ Pass/Source/View/CollectionView/BaseCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  BaseCollectionViewCell.swift
+//  Pass
+//
+//  Created by 강민석 on 2021/05/25.
+//
+
+import UIKit
+
+import RxSwift
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    
+    var disposeBag = DisposeBag()
+    
+    
+    // MARK: Initializing
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        self.init(frame: .zero)
+    }
+    
+}
