667fe5c889c63f0fa3fd9ef88ba33ff33b5625f8
KangMinseok <qbq5000@gmail.com>
2021-05-25 09:49:20 +0900

    💄 송금 대상자 지정 화면 추가

--- Pass/Source/ViewController/TransferDestination/TransferDestinationViewController.swift
+++ Pass/Source/ViewController/TransferDestination/TransferDestinationViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 import ReactorKit
+import ReusableKit
 
 final class TransferDestinationViewController: BaseViewController, View {
     
@@ -15,16 +16,25 @@ final class TransferDestinationViewController: BaseViewController, View {
     
     // MARK: Constants
     struct Metric {
-        
-    }
-    
-    struct Font {
-        
+        static let textFieldLeftRight = 20.f
+        static let textFieldTop = 70.f
+        static let textFieldHeight = 65.f
     }
 
     // MARK: - Properties
 
     // MARK: - UI
+    let doneButton = KeyboardTopButton()
+    
+    fileprivate let bankSelectField = PassTextField().then {
+        $0.textField.placeholder = "은행 선택"
+    }
+    
+    fileprivate lazy var accountNumberField = PassTextField().then {
+        $0.textField.placeholder = "계좌번호 입력"
+        $0.textField.keyboardType = .numberPad
+        $0.textField.inputAccessoryView = doneButton
+    }
 
     // MARK: - Initializing
     init(
@@ -32,6 +42,7 @@ final class TransferDestinationViewController: BaseViewController, View {
     ) {
         defer { self.reactor = reactor }
         super.init()
+        self.title = "누구에게 송금할까요?"
     }
 
     required init?(coder: NSCoder) {
@@ -42,17 +53,69 @@ final class TransferDestinationViewController: BaseViewController, View {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.addSubview(self.bankSelectField)
+        self.view.addSubview(self.accountNumberField)
     }
 
     override func setupConstraints() {
+        self.bankSelectField.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(30)
+            make.left.equalTo(Metric.textFieldLeftRight)
+            make.right.equalTo(-Metric.textFieldLeftRight)
+            make.height.equalTo(Metric.textFieldHeight)
+        }
         
+        self.accountNumberField.snp.makeConstraints { make in
+            make.top.equalTo(self.bankSelectField.snp.bottom).offset(20)
+            make.left.equalTo(Metric.textFieldLeftRight)
+            make.right.equalTo(-Metric.textFieldLeftRight)
+            make.height.equalTo(Metric.textFieldHeight)
+        }
     }
 
     // MARK: - Configuring
     func bind(reactor: Reactor) {
         // MARK: - input
+        let textFieldTap = self.bankSelectField.textField.rx.tapGesture().when(.recognized).share()
+        
+        textFieldTap
+            .map { _ in Reactor.Action.showBankList }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+//        textFieldTap
+//            .subscribe(onNext: { [weak self] _ in
+//                self?.bankSelectField.textField.endEditing(true)
+//            })
+//            .disposed(by: disposeBag)
+        
+        self.doneButton.rx.tap
+            .map { Reactor.Action.next }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
         
         // MARK: - output
+        reactor.state.map { $0.accountNumberValidation }
+            .distinctUntilChanged()
+            .bind(to: self.accountNumberField.rx.error)
+            .disposed(by: disposeBag)
         
+        reactor.state.map { $0.accountNumberValidation }
+            .distinctUntilChanged()
+            .bind(to: self.doneButton.rx.error)
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.bank }
+            .subscribe(onNext: { [weak self] bank in
+                self?.bankSelectField.textField.text = bank?.bankName
+            })
+            .disposed(by: disposeBag)
+        
+        // MARK: - view
+        self.doneButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                self?.view.endEditing(true)
+            })
+            .disposed(by: disposeBag)
     }
 }
--- Pass/Source/ViewController/TransferDestination/TransferDestinationViewReactor.swift
+++ Pass/Source/ViewController/TransferDestination/TransferDestinationViewReactor.swift
@@ -9,52 +9,74 @@ import ReactorKit
 import RxCocoa
 import RxSwift
 import RxFlow
+import SwiftMessages
 
 final class TransferDestinationViewReactor: Reactor, Stepper {
-    var steps = PublishRelay<Step>()
-    typealias Action = NoAction
-    struct State {}
-    let initialState: State = State()
-}
 
-//final class TransferDestinationViewReactor: Reactor, Stepper {
-//
-//    var steps = PublishRelay<Step>()
-//
-//    enum Action {
-//
-//    }
-//
-//    enum Mutation {
-//
-//    }
-//
-//    struct State {
-//
-//    }
-//
-//    let initialState: State
-//
-//    init() {
-//
-//    }
-//
-//    func mutate(action: Action) -> Observable<Mutation> {
-//        switch action {
-//        case <#pattern#>:
-//            <#code#>
-//        }
-//    }
-//
-//    func reduce(state: State, mutation: Mutation) -> State {
-//        var state = state
-//
-//        switch mutation {
-//        case <#pattern#>:
-//            <#code#>
-//        }
-//
-//        return state
-//    }
-//
-//}
+    var steps = PublishRelay<Step>()
+
+    enum Action {
+        case showBankList
+        case bankIsSelected(Bank)
+        case setAccountNumber(String)
+        case next
+    }
+
+    enum Mutation {
+        case setBank(Bank)
+        case checkAccountNumber(String)
+    }
+
+    struct State {
+        var bank: Bank?
+        var accountNumber: String = ""
+        
+        var accountNumberValidation: ValidationResult?
+    }
+
+    let initialState: State = State()
+    
+    func transform(action: Observable<Action>) -> Observable<Action> {
+        return Observable.merge(action, selectedBank.map(Action.bankIsSelected))
+    }
+
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .showBankList:
+            self.steps.accept(PassStep.bankListIsRequired)
+            return .empty()
+            
+        case let .setAccountNumber(accountNumber):
+            return Observable.just(Mutation.checkAccountNumber(accountNumber))
+            
+        case .next:
+            guard let bank = self.currentState.bank else {
+                SwiftMessages.show(config: Message.passConfig, view: Message.faildView("은행을 선택해주세요"))
+                return .empty()
+            }
+            
+            self.steps.accept(
+                PassStep.transferAmountIsRequired(bank, self.currentState.accountNumber)
+            )
+            return .empty()
+            
+        case let .bankIsSelected(bank):
+            return Observable.just(Mutation.setBank(bank))
+        }
+    }
+
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+
+        switch mutation {
+        case let .checkAccountNumber(accountNumber):
+            state.accountNumber = accountNumber
+            state.accountNumberValidation = accountNumber.validAccountNumber
+            
+        case let .setBank(bank):
+            state.bank = bank
+        }
+
+        return state
+    }
+}
